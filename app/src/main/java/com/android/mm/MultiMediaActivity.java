488a28ededc096fa8a4c107f878eaf5babe261d7
wangrl0303 <wangrl0303@thundersoft.com>
2018-12-17 09:46:08 +0800

    Create Multi-Media project.

---
+++ app/src/main/java/com/android/mm/MultiMediaActivity.java
@@ -0,0 +1,49 @@
+package com.android.mm;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.Arrays;
+import java.util.List;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Show a list using a RecyclerView.
+ */
+
+public class MultiMediaActivity extends ListActivity {
+
+    private static List<String> mDataSet = Arrays.asList(
+            "Grafika", ""
+    );
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_media_multi);
+        setActionBar(findViewById(R.id.toolbar));
+
+        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, mDataSet));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        try {
+            Class cls = Class.forName(getPackageName() + "." + mDataSet.get(position).toLowerCase() +
+                    "." + mDataSet.get(position) + "Activity");
+            startActivity(new Intent(this, cls));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/android/mm/grafika/GrafikaActivity.java
@@ -0,0 +1,7 @@
+package com.android.mm.grafika;
+
+import android.app.Activity;
+
+public class GrafikaActivity extends Activity {
+    
+}
