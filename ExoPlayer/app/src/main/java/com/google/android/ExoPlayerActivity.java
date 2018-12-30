c5793b629ae0cb7233a071df44cbae9d25ab2c9a
wangrl <wangrl2016@gmail.com>
2018-12-31 00:15:41 +0800

    Being new days, writing exoplayer begin to end.

---
+++ ExoPlayer/app/src/main/java/com/google/android/ExoPlayerActivity.java
@@ -0,0 +1,13 @@
+package com.google.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExoPlayerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exo_player);
+    }
+}
